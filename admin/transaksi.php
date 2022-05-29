3a733c4a2e7a1f2e7aa5ac4732d53ce781d6d0e4
audycipta02@gmail.com <audycipta02@gmail.com>
2022-05-29 23:17:57 +0800

    add search total bayar at dashboard admin (having)

--- admin/transaksi.php
+++ admin/transaksi.php
@@ -7,71 +7,85 @@
 
 <div class="d-flex" id="wrapper">
 
-	<!-- Sidebar -->
-	<?php include ROOT_PATH . 'includes/admin/sidebar.php'; ?>
+  <!-- Sidebar -->
+  <?php include ROOT_PATH . 'includes/admin/sidebar.php'; ?>
 
-	<!-- Page Content -->
-	<div id="page-content-wrapper">
+  <!-- Page Content -->
+  <div id="page-content-wrapper">
 
-		<!-- Navbar -->
-		<?php include ROOT_PATH . 'includes/admin/navbar.php'; ?>
+    <!-- Navbar -->
+    <?php include ROOT_PATH . 'includes/admin/navbar.php'; ?>
 
-		<div class="container-fluid my-3">
-			<div class="row">
-				<div class="col-md-9">
-					<?php flash('message') ?>
-					<div class="card">
-						<div class="card-header d-flex justify-content-between align-items-center">
-							<span>Transaksi Pelanggan</span>
-						</div>
-						<div class="card-body">
-							<table class="table table-striped mb-0">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">No. Pesanan</th>
-										<th scope="col">Nama Pelanggan</th>
-										<th scope="col">Aksi</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php if (mysqli_num_rows($result) > 0) : ?>
-										<?php $i = 1; ?>
-										<?php while ($row = mysqli_fetch_assoc($result)) : ?>
-											<tr>
-												<th scope="row" class="align-middle"><?= $i++ ?></th>
-												<td class="align-middle"><?= $row['no_pesanan'] ?></td>
-												<td class="align-middle"><?= $row['nama'] ?></td>
-												<td class="align-middle">
-													<a href="<?= base_url('admin/nota.php?no_pesanan=' . $row['no_pesanan']) ?>" class="badge badge-success">Nota</a>
-													<?php if ($row['status'] == 'Sedang Dipesan') : ?>
-														<a href="<?= base_url('admin/transaksi-bayar.php?no_pesanan=' . $row['no_pesanan']) ?>" class="badge badge-info">Bayar</a>
-													<?php elseif ($row['status'] == 'Lunas') : ?>
-														<form action="<?= base_url('queries/admin/transaksi-hapus.php') ?>" method="post" class="d-inline-block" onsubmit="return confirm('Yakin?')">
-															<input type="hidden" name="no_pesanan" value="<?= $row['no_pesanan'] ?>">
-															<button type="submit" class="badge badge-danger border-0">Hapus</button>
-														</form>
-													<?php endif ?>
-												</td>
-											</tr>
-										<?php endwhile ?>
-									<?php else : ?>
-										<tr>
-											<td colspan="4">
-												<div class="text-danger text-center">Pesanan kosong!</div>
-											</td>
-										</tr>
-									<?php endif ?>
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    <div class="container-fluid my-3">
+      <div class="row">
+        <div class="col-md-9">
+          <?php flash('message') ?>
+          <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+              <span>Transaksi Pelanggan</span>
+            </div>
+            <div class="card-body">
+              <form action="" method="POST">
+                <div class="form-group mb-2 row">
+                  <label for="search" class="col-sm-auto col-form-label">Total Bayar</label>
+                  <div class="col-sm-4">
+                    <input type="text" class="form-control-sm form-control" id="totalmin" name="totalmin" placeholder="Rp. Min">
+                  </div>
+                  <div class="col-sm-4">
+                    <input type="text" class="form-control-sm form-control" id="totalmax" name="totalmax" placeholder="Rp. Max">
+                  </div>
+                  <div class="col-sm-auto pl-0">
+                    <button type="submit" class="btn btn-primary btn-sm">Cari</button>
+                  </div>
+                </div>
+              </form>
+              <table class="table table-striped mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">No. Pesanan</th>
+                    <th scope="col">Nama Pelanggan</th>
+                    <th scope="col">Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php if (mysqli_num_rows($result) > 0) : ?>
+                    <?php $i = 1; ?>
+                    <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                      <tr>
+                        <th scope="row" class="align-middle"><?= $i++ ?></th>
+                        <td class="align-middle"><?= $row['no_pesanan'] ?></td>
+                        <td class="align-middle"><?= $row['nama'] ?></td>
+                        <td class="align-middle">
+                          <a href="<?= base_url('admin/nota.php?no_pesanan=' . $row['no_pesanan']) ?>" class="badge badge-success">Nota</a>
+                          <?php if ($row['status'] == 'Sedang Dipesan') : ?>
+                            <a href="<?= base_url('admin/transaksi-bayar.php?no_pesanan=' . $row['no_pesanan']) ?>" class="badge badge-info">Bayar</a>
+                          <?php elseif ($row['status'] == 'Lunas') : ?>
+                            <form action="<?= base_url('queries/admin/transaksi-hapus.php') ?>" method="post" class="d-inline-block" onsubmit="return confirm('Yakin?')">
+                              <input type="hidden" name="no_pesanan" value="<?= $row['no_pesanan'] ?>">
+                              <button type="submit" class="badge badge-danger border-0">Hapus</button>
+                            </form>
+                          <?php endif ?>
+                        </td>
+                      </tr>
+                    <?php endwhile ?>
+                  <?php else : ?>
+                    <tr>
+                      <td colspan="4">
+                        <div class="text-danger text-center">Pesanan kosong!</div>
+                      </td>
+                    </tr>
+                  <?php endif ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-	</div>
-	<!-- /#page-content-wrapper -->
+  </div>
+  <!-- /#page-content-wrapper -->
 
 </div>
 <!-- /#wrapper -->
--- queries/admin/transaksi.php
+++ queries/admin/transaksi.php
@@ -10,3 +10,17 @@ $query = "SELECT pesanan.*, pengguna.nama FROM pesanan
 		  WHERE pesanan.status IN ('Sedang Dipesan', 'Lunas')
 		  GROUP BY pesanan.no_pesanan";
 $result = mysqli_query($conn, $query);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$totalmin = $_POST['totalmin'];
+	$totalmax = $_POST['totalmax'];
+
+	$query = "SELECT SUM(menu.harga*jumlah) AS total, pesanan.*, pengguna.nama 
+			FROM pesanan
+			JOIN menu ON menu.id = pesanan.menu_id
+			JOIN pengguna ON pesanan.pengguna_id = pengguna.id 
+			WHERE pesanan.status IN ('Sedang Dipesan', 'Lunas')
+			GROUP BY no_pesanan
+			HAVING total > 51000 AND total < 100000;";
+	$result = mysqli_query($conn, $query);
+}
